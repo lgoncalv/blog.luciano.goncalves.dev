c585c6216f14dd2917ad6506bd2e0f0f76a91bcc
Luciano Goncalves <Luciano.Goncalves@buyagift.com>
2019-09-19 22:37:07 +0100

    Upgrade Angular and config firebase hosting

--- blog.luciano.goncalves.dev/src/app/app.component.spec.ts
+++ blog.luciano.goncalves.dev/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('lgblog');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lgblog!');
+    expect(compiled.querySelector('.content span').textContent).toContain('lgblog app is running!');
   });
 });
