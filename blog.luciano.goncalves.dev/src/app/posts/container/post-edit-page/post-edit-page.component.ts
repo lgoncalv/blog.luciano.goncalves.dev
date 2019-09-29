7606045049d2726cae1fa3398f8f7ae74a49f35f
Luciano Goncalves <Luciano.Goncalves@buyagift.com>
2019-09-29 08:17:33 +0100

    Start ost edit

---
+++ blog.luciano.goncalves.dev/src/app/posts/container/post-edit-page/post-edit-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lgblog-post-edit-page',
+  templateUrl: './post-edit-page.component.html',
+  styleUrls: ['./post-edit-page.component.scss']
+})
+export class PostEditPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
