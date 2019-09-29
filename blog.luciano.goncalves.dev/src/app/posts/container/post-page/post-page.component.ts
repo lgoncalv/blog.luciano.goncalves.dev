81c8d6557c6002b6a607608c3a1c7824f3a94686
Luciano Goncalves <Luciano.Goncalves@buyagift.com>
2019-09-29 08:15:58 +0100

    Change post page browser tile

--- blog.luciano.goncalves.dev/src/app/posts/container/post-page/post-page.component.ts
+++ blog.luciano.goncalves.dev/src/app/posts/container/post-page/post-page.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { PostService } from 'src/app/post.service';
 import { ActivatedRoute } from '@angular/router';
 import { Post } from '../../post';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'lgblog-post-page',
   templateUrl: './post-page.component.html'
 })
 export class PostPageComponent implements OnInit, OnDestroy {
-  post$: Observable<Post>;
+  post: Post;
 
   private subscriptions: Subscription[] = [];
 
-  constructor(private postService: PostService,
+  constructor(private titleService: Title,
+    private postService: PostService,
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.subscriptions.push(this.activatedRoute.params.subscribe(params => {
-      this.post$ = this.postService.getPost(params.slug);
+      this.subscriptions.push(this.postService.getPost(params.slug).subscribe(post => {
+        this.post = post;
+        this.titleService.setTitle(`LG's blog: ${this.post.title}`);
+      }))
     }, error => {
       console.log(`Error: ${error}`);
     }));
